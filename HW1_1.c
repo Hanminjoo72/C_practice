5672c82179e4e9823cdbeafdea7052223fac1407
Hanminjoo72 <132250663+Hanminjoo72@users.noreply.github.com>
2023-07-09 13:48:45 +0900

    programming

--- HW1_1.c
+++ HW1_1.c
@@ -1,44 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main() {
-    int num;
-   
-    scanf("%d", &num);
-
-    int found = 0;
-    for (int i = 2; i <= num / 2; i++) {
-        int is_prime_i = 1;
-        for (int j = 2; j <= i / 2; j++) {
-            if (i % j == 0) {
-                is_prime_i = 0;
-                break;
-            }
-        }
-        if (is_prime_i) {
-            int complement = num - i;
-            int is_prime_complement = 1;
-            if (complement >= 2) {
-                for (int j = 2; j <= complement / 2; j++) {
-                    if (complement % j == 0) {
-                        is_prime_complement = 0;
-                        break;
-                    }
-                }
-            }
-            else {
-                is_prime_complement = 0;
-            }
-            if (is_prime_complement) {
-                printf("%d + %d = %d\n", i, complement, num);
-                found = 1;
-            }
-        }
-    }
-    if (!found) {
-        printf("no expression.\n");
-    }
-    return 0;
+#include <stdio.h> 
+int main(void)
+{
+	printf("Photoshop\tFun\n");
+	printf("Java Script\tEasy\n");
+	printf("Java\t\tPowerful\n");
+	printf("C++\t\tInteresting\n");
+	printf("C\t\tVery Important\n");
+}
 }
 
 
